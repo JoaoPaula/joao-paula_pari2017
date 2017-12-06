882c576d7d969e446cc4f711a2205c1d5c979d06
Joao Paula <jcpaula@ua.pt>
2017-12-06 12:20:26 +0000

    lalalal

---
+++ build/CMakeFiles/myf.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myf.dir/scr/main.c.o"
+  "CMakeFiles/myf.dir/scr/myf.c.o"
+  "CMakeFiles/myf.dir/scr/callbacks.c.o"
+  "myf.pdb"
+  "myf"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/myf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
