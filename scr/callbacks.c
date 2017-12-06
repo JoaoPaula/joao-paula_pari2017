3167eb59e545b52b83464f293fb35334280ddf35
Joao Paula <jcpaula@ua.pt>
2017-12-06 10:59:54 +0000

    lal

--- scr/callbacks.c
+++ scr/callbacks.c
@@ -147,3 +147,11 @@ void p_InitTimer()
 
 /////////////////////////////Aula12/////////////////////////////////
 /////////////////////////////////////////////////////////////////////
+
+gboolean pari_UpdateImageAreas(gpointer data)
+{
+        //generate an expose event (draw event) on drawingarea1
+        GtkWidget *da1 = GTK_WIDGET(gtk_builder_get_object(builderG, "drawingarea1"));
+        gtk_widget_queue_draw(da1);
+        return TRUE;
+}
--- scr/main.c
+++ scr/main.c
@@ -29,15 +29,21 @@ int main(int argc, char *argv[])
 
         /* load the interface after a configuration file*/
         builderG = gtk_builder_new();
-        gtk_builder_add_from_file(builderG, "Aula11.glade", NULL);
+        gtk_builder_add_from_file(builderG, "mydr2.glade", NULL);
 
         /* connect the signals in the interface */
         gtk_builder_connect_signals(builderG, NULL);
 
         /* get main window Widget ID and connect special signals */
         GtkWidget *t = GTK_WIDGET(gtk_builder_get_object(builderG, "window1"));
-        if(t)
+
+	g_timeout_add(30, (GSourceFunc) pari_UpdateImageAreas, (gpointer) NULL);
+        captureG=pari_StartImageAcquisition();
+        
+	if(t)
         {
+
+
                 g_signal_connect(G_OBJECT(t), "delete_event", G_CALLBACK(pari_delete_event), NULL);
         }
 
@@ -48,6 +54,10 @@ int main(int argc, char *argv[])
 
         /* start the event loop */
         gtk_main();
+
+	cvReleaseCapture(&captureG);             //Release capture device.
+        cvReleaseImage(&dst_imageG);             //Release image (free pointer when no longer used)
+        cvReleaseImage(&src_imageG);             //Release image (free pointer when no longer used).
 
         return 0;
 
